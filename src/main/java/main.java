2e439a2bebf1fd0824229d6aaf2467e88ee0f072
Isaac Perez <20130824@ce.pucmm.edu.do>
2017-05-19 11:44:41 -0400

    Cambios del primer dia de clase

---
+++ src/main/java/main.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Isaac Perez on 19/5/2017.
+ */
+public class main {
+    public static void main(String[] args) {
+        System.out.printf("Hola mundo en Java D:");
+    }
+
+
+}
